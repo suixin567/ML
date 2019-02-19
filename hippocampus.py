fada8d1677f5d6ab439133ad157114c07f5d07fb
ljj <591821945@qq.com>
2019-02-19 16:23:14 +0800

    创建海马，接下来准备连接sql

---
+++ hippocampus.py
@@ -0,0 +1,71 @@
+# -*- coding: UTF-8 -*-
+class ShallowMemory:
+    f1 = 0;
+    intensity = 0;
+
+    # 创建一条新的浅记忆
+    def __init__(self, f1):
+        self.f1 = f1;
+
+
+    def __init__(self, value):
+        self.f1 = value;
+
+
+
+
+class Hippocampus:
+
+    toDeep = 5#转为深记忆阈值
+    shallowMemorys = []
+
+    def collect(self,feature):
+        Hf, Wf = feature.shape
+        count=0;
+        for i in range(Hf):
+            for j in range(Wf):
+                count+= feature[i][j];
+        #分档
+        if(count<50):
+            self.check(0);
+            return;
+        if ( 50<=count and count <100):
+            self.check(50);
+            return;
+        if (100 <= count and count < 150):
+            self.check(100);
+            return;
+        if (150 <= count and count < 200):
+            self.check(150);
+            return;
+        if (200 <= count and count < 250):
+            self.check(200);
+            return;
+        if (250 <= count and count < 300):
+            self.check(250);
+            return;
+        if (300 <= count):
+            self.check(300);
+            return;
+
+    def check(self,newValue):
+        # 判断是否已经存在此记忆
+        isExist = False;
+        for i in range(len(self.shallowMemorys)):
+            if newValue == self.shallowMemorys[i].f1:
+                self.shallowMemorys[i].intensity+=1;
+                print("+++++++++++++++++++++++++++++++>>>", newValue);
+                if(self.shallowMemorys[i].intensity>=self.toDeep):
+                    print("这个记忆转为深记忆",self.shallowMemorys[i].f1)
+                isExist = True;
+        if(isExist == False):
+            print("----------------------------->>>", newValue);
+            newMemery = ShallowMemory(newValue);
+            self.shallowMemorys.append(newMemery);
+
+
+    def save(self):
+        print("保存海马体",self.toDeep);
+
+
+
--- train.py
+++ train.py
@@ -3,8 +3,8 @@ import cv2
 import numpy as np
 import glob
 import os
-import tool
-
+import tools.tool
+import hippocampus
 
 train_path='training_data'
 imgWidth=500
@@ -17,7 +17,7 @@ def foo():
     print("文件", files)
     lastimg = np.zeros((imgHeight,imgWidth))
     activateimg = np.zeros((imgHeight,imgWidth))
-
+    hipp = hippocampus.Hippocampus();
     for fl in files:
         image = cv2.imread(fl)
         print("原图尺寸",image.shape);
@@ -29,20 +29,27 @@ def foo():
         lastimg = rimg
         activateimg = activateimg.astype(np.uint8)#此句不可以省略。
         #print(activateimg);
+
         cv2.imshow('activate', activateimg)
+        cv2.moveWindow("activate", 0, 0)
         cv2.waitKey(1000)
         #print(activateimg);
 
         # cv2.imshow('单通道图像', b)
         # cv2.waitKey(1000)
-        res = tool.conv_same(activateimg,kernel)
+        #获取特征
+        feature = tools.tool.conv_same(activateimg,kernel)
         #print("竖向检测完成后",res);
 
-        res = np.clip(res,0,255)
+        feature = np.clip(feature,0,255)
         #res2 = res2.astype(np.uint8)
         #print("截取后", res2)
-        cv2.imshow('v-result', res)
+        cv2.imshow('v-result', feature)
+        cv2.moveWindow("v-result", 0, 350)
         cv2.waitKey(1000)
+        hipp.collect(feature);
+    #持久化海马体
+    hipp.save();
 
 if __name__=="__main__":
     foo();

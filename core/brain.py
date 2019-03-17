19a4de5044a4bb4fee83207708f6b80676bdabca
suixin_pc <591821945@qq.com>
2019-03-17 11:20:30 +0800

    独立元向前传递的方式，但还有一个bug.

--- core/brain.py
+++ core/brain.py
@@ -18,32 +18,38 @@ class Brain:
 
     def receive(self,pointer):
         print("脑开始运行...")#此list的具体内容： ['15_corner_', '13_vertical_']
-        stepIndex=0
+
         featureList = g.r.lrange(pointer, 0,g.r.llen(pointer))  # 获取记忆的具体内容 ['15_corner_', '13_vertical_']
+        #传给第一排元
+        self.transmitMethod(featureList,0)
+
+        #传递接下来每一排的元
+        for i in range(1,10):
+            print("准备传递至第", i, "层")
+            for j in range(10):
+                neure=self.neures[i][j]
+                #获取这个元的特征列表
+                nuure_features = g.r.keys(pattern='neure' + str(neure.id) + '*')
+                self.transmitMethod(nuure_features,i)
+
+    #单个元的传递规则 根据特征List向下传递一层
+    def  transmitMethod(self,featureList,stepIndex):
+
         for f in featureList:
-            #向元传递
+            #向下传递（对每一个特征都要找到最应该传递的目标元）
             familiar_neure =None#最想传给的元
             familiar_max = -1#记录哪个元最熟悉这个特征
-            #遍历第一排元
+            #遍历下一排元
             for j in range(10):
                 # print(self.neures[i][j].id)
-                familiar_intensity = self.neures[0][j].familiar(f)#获得熟悉程度
+                familiar_intensity = self.neures[stepIndex][j].familiar(f)#获得熟悉程度
                 if familiar_intensity > familiar_max:
                     familiar_max = familiar_intensity
-                    familiar_neure = self.neures[0][j]
+                    familiar_neure = self.neures[stepIndex][j]
             if familiar_max ==-1:
-                print("没有熟悉的元")
-                self.neures[0][random.randint(0, 9)].receive(f,intensity=10)
-                # self.neures[11].receive(pointer)
+                #print("没有熟悉的元")
+                self.neures[stepIndex][random.randint(0, 9)].receive(f,intensity=10)
             else:
-                print("找到了最熟悉的元")
+                #print("找到了最熟悉的元")
                 familiar_neure.receive(f,intensity=10)
 
-        #运行到这里时，对每一个特征都传递给了第一层的元。
-        print("已经传给了元，应通知元继续传递一步。")
-
-        for m in range(10):
-            for n in range(10):
-                self.neures[stepIndex][n].forward()
-            #全向前传递一步后，计数加一。
-            stepIndex = stepIndex+1
--- core/neure.py
+++ core/neure.py
@@ -14,10 +14,6 @@ class Neure:
     def receive(self,feature,intensity):
         print("元",self.id," 接收：",feature,"强度",intensity)
         self.activate =True
-
-
-
-
         #记录一个特征
         #先获取这个特征的intensity
         intensity = 0
@@ -32,12 +28,6 @@ class Neure:
         #传递到皮层
         # self.pallium.receive(_from)
 
-    # def send(self):
-    #     print("发送")
-    #     #记录自己发给谁
-    #     # g.r.set("neure"+self.id,id)
-    #     self.activate = False
-
     #判断对某个特征的熟悉程度
     def familiar(self,feature): #feature的样子： 13_vertical_
         # print("特征的样子",feature)
@@ -48,27 +38,7 @@ class Neure:
         else:
             return int(result)
 
-    def forward(self):
-        if self.activate:
-            print("我该向前一步了",self.id)
-            #遍历自己所有特征
-            myfeatures =g.r.keys(pattern='neure'+str(self.id)+'*')
-            print(myfeatures)
-            for f in myfeatures:
-                # 遍历下一排元
-                for j in range(10):
-                    # print(self.neures[i][j].id)
-                    familiar_intensity = self.neures[0][j].familiar(f)  # 获得熟悉程度
-                    if familiar_intensity > familiar_max:
-                        familiar_max = familiar_intensity
-                        familiar_neure = self.neures[0][j]
-                if familiar_max == -1:
-                    print("没有熟悉的元")
-                    self.neures[0][random.randint(0, 9)].receive(f, intensity=10)
-                    # self.neures[11].receive(pointer)
-                else:
-                    print("找到了最熟悉的元")
-                    familiar_neure.receive(f, intensity=10)
+
 
 
 
